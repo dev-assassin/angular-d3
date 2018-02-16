425d96743103caaf20e105312bad85379020bc02
ninjadev1030 <denis.dovganiuc@gmail.com>
2018-02-16 11:31:47 +0200

    add d3 library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import * as d3 from 'd3';
+import * as d3Scale from 'd3-scale';
+import * as d3Shape from 'd3-shape';
+import * as d3Array from 'd3-array';
+import * as d3Axis from 'd3-axis';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  private margin = { top: 20, right: 20, bottom: 30, left: 50 };
+  private width: number;
+  private height: number;
+  private x: any;
+  private y: any;
 }
